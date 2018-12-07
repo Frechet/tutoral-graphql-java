a4d20c404659b3d9e7f6baeaa2eb8dd6d6c0caee
Victor Borovlev <borovl.victor@gmail.com>
2018-12-07 23:40:57 +0600

    init schema

---
+++ src/main/java/graphql/java/tutorial/controller/GraphQLEndpoint.java
@@ -0,0 +1,18 @@
+package graphql.java.tutorial.controller;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import javax.servlet.annotation.WebServlet;
+import graphql.servlet.SimpleGraphQLServlet;
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(SchemaParser.newParser()
+                .file("schema.graphqls") //parse the schema file created earlier
+                .build()
+                .makeExecutableSchema());
+    }
+}
+
